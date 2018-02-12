21dffa05e95156682c9432bc4dc6be476df7f4e7
James Ferguson <ncjamesferguson@gmail.com>
2018-02-12 18:07:19 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,19 @@
 class Team
 
+  @@teams = []
+
+  attr_accessor :name, :motto
+
+  def initialize(args)
+    @name = args[:name]
+    @motto = args[:motto]
+    @@teams << self
+  end
+
+  def self.all
+    @@teams
+  end
+
+
+
 end

ee79d50c05f794675fa5570d7621fddaffdadd2a
James Ferguson <ncjamesferguson@gmail.com>
2018-02-12 18:05:12 +0000

    automatically backed up by learn

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,3 +1,15 @@
 class Superhero
 
+  @@superheroes = []
+
+  attr_accessor :name, :motto
+
+  def intialize(args)
+    args.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    @@superheroes << self
+  end
+    
+
 end

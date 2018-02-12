5458f279fa1a2f600ca18b9571e0fde320b1b6af
James Ferguson <ncjamesferguson@gmail.com>
2018-02-12 18:21:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,13 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
+      @team = Team.new(params[:team])
+
+      params[:team][:heroes].each do |details|
+        Hero.new(details)
+      end
+
+      @heroes = Hero.all
 
       erb :team
     end
